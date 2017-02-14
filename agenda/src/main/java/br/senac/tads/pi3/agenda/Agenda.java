b7bbb7a754123120d70a07b76b355ce8d8d143e2
Fernando Tsuda <fernando.tsuda@sp.senac.br>
2017-02-14 20:40:17 -0200

    Inclusão do driver JDBC e método de conexão

--- agenda/src/main/java/br/senac/tads/pi3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/agenda/Agenda.java
@@ -23,11 +23,29 @@
  */
 package br.senac.tads.pi3.agenda;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author fernando.tsuda
  */
 public class Agenda {
+
+  protected Connection obterConexao() throws SQLException, ClassNotFoundException {
+    Connection conn = null;
+
+    // Passo 1: Registrar o driver JDBC
+    Class.forName("org.apache.derby.jdbc.ClientDataSource");
+
+    // Passo 2: Abrir a conexão
+    conn = DriverManager.getConnection(
+	    "jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+	    "app", // usuário BD
+	    "app"); // senha BD
+    return conn;
+  }
 
   public static void main(String[] args) {
     System.out.println("Olá mundo");
